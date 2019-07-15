a82cdf01b5508be3a4b611c8a1bfdbc4e3adfc31
Kevin <43480532+KevinGK93@users.noreply.github.com>
2019-07-15 03:53:33 +0200

    Practicum files opnieuw toevoegen

---
+++ Practicum/WAC/src/practicum6/Server.java
@@ -0,0 +1,17 @@
+package practicum6;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+	public static void main(String[] args) throws Exception {
+		while (true) {
+			ServerSocket ss = new ServerSocket(4711);
+			Socket s = ss.accept();
+			MyServlet MyServlet = new MyServlet(s);
+			MyServlet.start();
+
+			ss.close();
+		}
+	}
+}
